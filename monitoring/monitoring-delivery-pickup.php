21c7caf8bedeb7fc36993395c68000a1b6e1f489
hercu023 <rapquijano04@gmail.com>
2023-01-03 07:01:06 +0800

    feat: print

--- monitoring/monitoring-delivery-pickup.php
+++ monitoring/monitoring-delivery-pickup.php
@@ -2724,7 +2724,7 @@ table tbody td{
                                                 <th>QTY</th>
                                                 <th>AMOUNT</th>
                                                 <th>     
-                                                    <a href="../service/delete-transaction-order.php?delete-list=<?php echo $transactions_uuid['uuid']; ?>" class="delete-rowsButton" class="action-btn" name="action">
+                                                    <a href="../service/delete-transaction-order.php?delete-list=<?php echo $user_id; ?>" class="delete-rowsButton" class="action-btn" name="action">
                                                         X
                                                     </a>
                                                 </th>
@@ -2792,10 +2792,11 @@ table tbody td{
                             
         
                             <div class="receipt-buttons">
-                                <button type="submit" class="confirmOrder-button" name="print" onclick="print();">
+                                <a href="../monitoring/monitoring-delivery-pickup-list-receipt.php?uuid=<?php echo $uuid?>" id="addcustomerBtn" class="confirmOrder-button">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M15 6H5V3h10Zm-.25 4.5q.312 0 .531-.219.219-.219.219-.531 0-.312-.219-.531Q15.062 9 14.75 9q-.312 0-.531.219Q14 9.438 14 9.75q0 .312.219.531.219.219.531.219Zm-1.25 5v-3h-7v3ZM15 17H5v-3H2V9q0-.833.583-1.417Q3.167 7 4 7h12q.833 0 1.417.583Q18 8.167 18 9v5h-3Z"/></svg>
                                     Print
-                                </button>
+                                </a>
+                                
                                 <button type="submit" class="confirmOrder-button2" name="deliver">
                                     DELIVER
                                 </button>
