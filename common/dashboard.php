007d5f6634c0ee833103e478e0b5396a87ee420c
hercu023 <rapquijano04@gmail.com>
2023-01-05 05:40:54 +0800

    design

--- common/dashboard.php
+++ common/dashboard.php
@@ -25,6 +25,198 @@ if (!get_user_access_per_module($con, $_SESSION['user_user_type'], 'DASHBOARD'))
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>   
     </head>
     <style>
+        /* ----------------------------TOP MENU---------------------------- */
+
+.top-menu a{
+    text-decoration: none;
+}
+.top-menu{
+    margin-top: 1rem;
+    position: absolute;
+    right: 4%;
+}
+.top-menu .menu-bar{
+    display: flex;
+    justify-content: end;
+    gap: 2rem;
+}
+.top-menu .menu-bar button{
+    display: none;
+}
+.top-menu .menu-bar .user1{
+    gap: 2rem;
+    align-items: right;
+    text-align: right;
+}
+.top-menu .menu-bar .user2{
+    display: flex;
+    gap: 2rem;
+    align-items: right;
+    text-align: right;
+}
+.top-menu .menu-bar .accTitle-top{
+    font-size: min(max(1.2rem, 0.4vw), 1.3rem);
+    color: var(--color-main); 
+    font-family: 'COCOGOOSE', sans-serif;
+    letter-spacing: .03rem;
+    display: none;
+    text-align: center;
+    align-items: center;
+}
+.user-type{
+    font-family: 'switzer', sans-serif;
+    font-size: 7.5px;
+    color: var(--color-black); 
+    letter-spacing: 1px;
+    border-top: 2px solid var(--color-main); 
+    margin-top: -0.97rem;
+    text-transform: uppercase;
+}
+h1{
+    margin-top: 6px;     
+}
+.welcome{
+    font-family: 'Calibri', sans-serif;
+    font-size: 11px;
+    /* margin-right: -7.3rem;*/
+    margin-top: -0.6rem; 
+    letter-spacing: 1px;
+    color: var(--color-main); 
+}
+.user-name{
+    font-family: 'Switzer', sans-serif;
+    font-size: 12px;
+    margin-top: -1rem; 
+    text-transform: uppercase;
+    margin-bottom: 0;
+    color: var(--color-maroon);
+}
+.profile img{
+    background: var(--color-white); 
+    border-radius: 30%;
+    width: 50px;
+    padding: 4px;
+    margin-top: .3rem;
+}
+
+.user2 .profile{
+    position: relative;
+    cursor: pointer;
+}
+.user2 .drop-menu{
+    position: absolute;
+    top: 120px;
+    right: 0;
+    padding: 10px 20px;
+    background: var(--color-white);
+    box-shadow: 3px 2px 10px 1px var(--color-solid-gray);
+    width: 110px;
+    box-sizing: 0 5px 25px rgba(0,0,0,0.1);
+    border-radius: 7px;
+    transition: 0.5s;
+    visibility: hidden;
+    opacity: 0;
+}
+.user2 .drop-menu.user2{
+    top: 85px;
+    visibility: visible;
+    opacity: 1;
+}
+.user2 .drop-menu::before{
+    content:'';
+    position: absolute;
+    top: -5px;
+    right: 25px;
+    width: 15px;
+    height: 20px;
+    background: var(--color-white);
+    transform: rotate(45deg);
+    transition: 0.5s;
+}
+.drop-menu .ul .user-type3{
+    font-family: 'Calibri', sans-serif;
+    font-size: 7.5px;
+    color: var(--color-main);
+    letter-spacing: .2rem;
+    display: none;
+    text-transform: uppercase;
+}
+
+.drop-menu .ul{
+    display: flex;
+    flex-direction: column;
+    height: 5rem;
+    position: relative;
+    margin-bottom: 0.5rem;
+}
+.drop-menu h4{
+    font-weight: 400;
+    font-size: 12px;
+}
+.drop-menu .ul a{
+    display: flex;
+    color: hsl(0, 0%, 69%);
+    fill: hsl(0, 0%, 69%);
+    margin-left: -1.26rem;
+    padding-left: 1rem;
+    gap: 1rem;
+    height: 1rem;
+    width: 8.5rem;
+    align-items: center;
+    position: relative;
+    height: 1.7rem;
+    transition: all 300ms ease;
+}
+.drop-menu .ul a:hover {
+    background:  rgb(190, 190, 190);
+    transition: 0.6s;
+    color: var(--color-white);
+    fill: var(--color-white);
+    padding-left: .9rem;
+    content: "";
+    margin-bottom: 6px;
+    font-size: 15px;
+    border-radius: 0px 0px 10px 10px;
+    cursor: pointer;
+}
+.checkbox{
+    opacity: 0;
+    position: absolute;
+}
+.checkbox:checked + .theme-dark .ball{
+    transform: translateX(28px);
+}
+.drop-menu .theme-dark{
+    background: hsl(0, 0%, 69%);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 14.5px;
+    width: 42.5px;
+    cursor: pointer;
+    border-radius: 50px;
+    position: relative;
+    padding: 5px;
+    margin-top: -30px;
+    margin-bottom: 8px;
+    margin-left: 2rem;
+}
+.sun{
+    fill: yellow;
+}
+.moon{
+    fill: white;
+}
+.ball{
+    background: white;
+    position: absolute;
+    border-radius: 50%;
+    top: 2px;
+    left: 2px;
+    height: 21px;
+    width: 21px;
+    transition: transform 0.2s linear;
+}
 :root{
     --color-main: rgb(2, 80, 2);
     --color-white: white;
@@ -1891,30 +2083,54 @@ h3{
                                 <div class="user-type3">
                                     <h1><?php echo $_SESSION['user_user_type']; ?> </h1>
                                 </div>
-                                <input type="checkbox" class="checkbox" id="checkbox">
-                                <label for="checkbox" class="theme-dark">
-                                    <svg class="moon" xmlns="http://www.w3.org/2000/svg" height="18" width="18"><path d="M10 17q-2.917 0-4.958-2.042Q3 12.917 3 10q0-2.917 2.042-4.958Q7.083 3 10 3q.271 0 .531.021.261.021.531.062-.812.605-1.291 1.5-.479.896-.479 1.917 0 1.771 1.218 2.99 1.219 1.218 2.99 1.218 1.021 0 1.917-.479.895-.479 1.5-1.291.041.27.062.531.021.26.021.531 0 2.917-2.042 4.958Q12.917 17 10 17Z"/></svg>
-                                    <svg class="sun" xmlns="http://www.w3.org/2000/svg" height="18" width="18"><path d="M10 14q-1.667 0-2.833-1.167Q6 11.667 6 10q0-1.667 1.167-2.833Q8.333 6 10 6q1.667 0 2.833 1.167Q14 8.333 14 10q0 1.667-1.167 2.833Q11.667 14 10 14Zm-8.25-3.25q-.312 0-.531-.219Q1 10.312 1 10q0-.312.219-.531.219-.219.531-.219h2q.312 0 .531.219.219.219.219.531 0 .312-.219.531-.219.219-.531.219Zm14.5 0q-.312 0-.531-.219-.219-.219-.219-.531 0-.312.219-.531.219-.219.531-.219h2q.312 0 .531.219Q19 9.688 19 10q0 .312-.219.531-.219.219-.531.219ZM10 4.5q-.312 0-.531-.219-.219-.219-.219-.531v-2q0-.312.219-.531Q9.688 1 10 1q.312 0 .531.219.219.219.219.531v2q0 .312-.219.531-.219.219-.531.219ZM10 19q-.312 0-.531-.219-.219-.219-.219-.531v-2q0-.312.219-.531.219-.219.531-.219.312 0 .531.219.219.219.219.531v2q0 .312-.219.531Q10.312 19 10 19ZM5.042 6.104 4 5.042q-.229-.209-.229-.511 0-.302.229-.531.208-.229.521-.229.312 0 .521.229l1.062 1.042q.229.229.229.531 0 .302-.229.531-.208.229-.521.229-.312 0-.541-.229ZM14.958 16l-1.062-1.042q-.229-.229-.229-.531 0-.302.229-.531.208-.229.521-.229.312 0 .541.229L16 14.958q.229.209.229.511 0 .302-.229.531-.229.229-.521.229-.291 0-.521-.229Zm-1.062-9.896q-.229-.208-.229-.521 0-.312.229-.541L14.958 4q.23-.229.521-.219.292.011.521.219.229.229.229.521 0 .291-.229.521l-1.042 1.062q-.229.229-.531.229-.302 0-.531-.229ZM4 16q-.229-.208-.229-.521 0-.312.229-.521l1.042-1.062q.229-.208.531-.208.302 0 .531.208.229.229.219.531-.011.302-.219.531L5.042 16q-.209.229-.511.229-.302 0-.531-.229Z"/></svg>
-                                    <div class="ball"></div>
-                                </label>
-                                </input>
-                                <a href="#" class="account">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M4.917 14.167q1.062-.875 2.364-1.313 1.302-.437 2.719-.437 1.417 0 2.719.437 1.302.438 2.385 1.313.688-.855 1.084-1.907.395-1.052.395-2.26 0-2.75-1.916-4.667Q12.75 3.417 10 3.417T5.333 5.333Q3.417 7.25 3.417 10q0 1.208.406 2.26.406 1.052 1.094 1.907ZM10 10.854q-1.229 0-2.073-.844-.844-.843-.844-2.072 0-1.23.844-2.073.844-.844 2.073-.844t2.073.844q.844.843.844 2.073 0 1.229-.844 2.072-.844.844-2.073.844Zm0 7.479q-1.729 0-3.25-.656t-2.646-1.781q-1.125-1.125-1.781-2.646-.656-1.521-.656-3.25t.656-3.25q.656-1.521 1.781-2.646T6.75 2.323q1.521-.656 3.25-.656t3.25.656q1.521.656 2.646 1.781t1.781 2.646q.656 1.521.656 3.25t-.656 3.25q-.656 1.521-1.781 2.646t-2.646 1.781q-1.521.656-3.25.656Zm.021-1.75q1.021 0 2-.312.979-.313 1.771-.896-.771-.604-1.75-.906-.98-.302-2.042-.302-1.062 0-2.031.302-.969.302-1.761.906.792.583 1.782.896.989.312 2.031.312ZM10 9.104q.521 0 .844-.323.323-.323.323-.843 0-.521-.323-.844-.323-.323-.844-.323-.521 0-.844.323-.323.323-.323.844 0 .52.323.843.323.323.844.323Zm0-1.166Zm0 7.437Z"/></svg>
-                                    <h4>My Account</h4>
-                                </a>
-                                <a href="#" class="help">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M10 15q.417 0 .708-.292Q11 14.417 11 14t-.292-.708Q10.417 13 10 13t-.708.292Q9 13.583 9 14t.292.708Q9.583 15 10 15Zm-.75-3.188h1.521q0-.77.135-1.093.136-.323.656-.823.73-.708 1.011-1.208.281-.5.281-1.105 0-1.145-.781-1.864Q11.292 5 10.083 5q-1.062 0-1.843.562-.782.563-1.094 1.521l1.354.563q.188-.584.594-.906.406-.323.948-.323.583 0 .958.333t.375.875q0 .479-.323.854t-.719.729q-.729.667-.906 1.094-.177.427-.177 1.51ZM10 18q-1.646 0-3.104-.625-1.458-.625-2.552-1.719t-1.719-2.552Q2 11.646 2 10q0-1.667.625-3.115.625-1.447 1.719-2.541Q5.438 3.25 6.896 2.625T10 2q1.667 0 3.115.625 1.447.625 2.541 1.719 1.094 1.094 1.719 2.541Q18 8.333 18 10q0 1.646-.625 3.104-.625 1.458-1.719 2.552t-2.541 1.719Q11.667 18 10 18Zm0-1.5q2.708 0 4.604-1.896T16.5 10q0-2.708-1.896-4.604T10 3.5q-2.708 0-4.604 1.896T3.5 10q0 2.708 1.896 4.604T10 16.5Zm0-6.5Z"/></svg>
-                                    <h4>Help</h4>
-                                </a>
-                                <a href="../auth/logout.php" class="logout">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M4.5 17q-.625 0-1.062-.438Q3 16.125 3 15.5v-11q0-.625.438-1.062Q3.875 3 4.5 3H10v1.5H4.5v11H10V17Zm9-3.5-1.062-1.062 1.687-1.688H8v-1.5h6.125l-1.687-1.688L13.5 6.5 17 10Z"/></svg>
-                                    <h4>Logout</h4>
-                                </a>
+                                <div class="user-type4">
+                                    <?php
+                                    $query = "SELECT 
+                                    users.user_id,
+                                    users.last_name,
+                                    users.first_name,
+                                    users.middle_name,
+                                    users.email,
+                                    users.contact_number, 
+                                    users.profile_image, 
+                                    account_type.user_type, 
+                                    status_archive.status 
+                                    FROM users 
+                                    INNER JOIN account_type 
+                                    ON users.account_type_id = account_type.id 
+                                    INNER JOIN status_archive 
+                                    ON users.status_archive_id = status_archive.id
+                                    WHERE users.status_archive_id = '1'
+                                    ORDER BY users.user_id";
+                                    $result = mysqli_query($con, $query);
+                                    if ($rows = mysqli_fetch_assoc($result))
+                                    {
+                                        ?>
+                                    <a href="../accounts/account-view.php?view=<?php echo $_SESSION['user_user_id']; ?>" class="account">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M4.917 14.167q1.062-.875 2.364-1.313 1.302-.437 2.719-.437 1.417 0 2.719.437 1.302.438 2.385 1.313.688-.855 1.084-1.907.395-1.052.395-2.26 0-2.75-1.916-4.667Q12.75 3.417 10 3.417T5.333 5.333Q3.417 7.25 3.417 10q0 1.208.406 2.26.406 1.052 1.094 1.907ZM10 10.854q-1.229 0-2.073-.844-.844-.843-.844-2.072 0-1.23.844-2.073.844-.844 2.073-.844t2.073.844q.844.843.844 2.073 0 1.229-.844 2.072-.844.844-2.073.844Zm0 7.479q-1.729 0-3.25-.656t-2.646-1.781q-1.125-1.125-1.781-2.646-.656-1.521-.656-3.25t.656-3.25q.656-1.521 1.781-2.646T6.75 2.323q1.521-.656 3.25-.656t3.25.656q1.521.656 2.646 1.781t1.781 2.646q.656 1.521.656 3.25t-.656 3.25q-.656 1.521-1.781 2.646t-2.646 1.781q-1.521.656-3.25.656Zm.021-1.75q1.021 0 2-.312.979-.313 1.771-.896-.771-.604-1.75-.906-.98-.302-2.042-.302-1.062 0-2.031.302-.969.302-1.761.906.792.583 1.782.896.989.312 2.031.312ZM10 9.104q.521 0 .844-.323.323-.323.323-.843 0-.521-.323-.844-.323-.323-.844-.323-.521 0-.844.323-.323.323-.323.844 0 .52.323.843.323.323.844.323Zm0-1.166Zm0 7.437Z"/></svg>
+                                        <h4>My Account</h4>
+                                    </a>
+                                <?php }?>
+
+                                    <a href="../settings/settings-help.php" class="help">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M10 15q.417 0 .708-.292Q11 14.417 11 14t-.292-.708Q10.417 13 10 13t-.708.292Q9 13.583 9 14t.292.708Q9.583 15 10 15Zm-.75-3.188h1.521q0-.77.135-1.093.136-.323.656-.823.73-.708 1.011-1.208.281-.5.281-1.105 0-1.145-.781-1.864Q11.292 5 10.083 5q-1.062 0-1.843.562-.782.563-1.094 1.521l1.354.563q.188-.584.594-.906.406-.323.948-.323.583 0 .958.333t.375.875q0 .479-.323.854t-.719.729q-.729.667-.906 1.094-.177.427-.177 1.51ZM10 18q-1.646 0-3.104-.625-1.458-.625-2.552-1.719t-1.719-2.552Q2 11.646 2 10q0-1.667.625-3.115.625-1.447 1.719-2.541Q5.438 3.25 6.896 2.625T10 2q1.667 0 3.115.625 1.447.625 2.541 1.719 1.094 1.094 1.719 2.541Q18 8.333 18 10q0 1.646-.625 3.104-.625 1.458-1.719 2.552t-2.541 1.719Q11.667 18 10 18Zm0-1.5q2.708 0 4.604-1.896T16.5 10q0-2.708-1.896-4.604T10 3.5q-2.708 0-4.604 1.896T3.5 10q0 2.708 1.896 4.604T10 16.5Zm0-6.5Z"/></svg>
+                                        <h4>Help</h4>
+                                    </a>
+                                    <a href="../auth/logout.php" class="logout">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M4.5 17q-.625 0-1.062-.438Q3 16.125 3 15.5v-11q0-.625.438-1.062Q3.875 3 4.5 3H10v1.5H4.5v11H10V17Zm9-3.5-1.062-1.062 1.687-1.688H8v-1.5h6.125l-1.687-1.688L13.5 6.5 17 10Z"/></svg>
+                                        <h4>Logout</h4>
+                                    </a>
+                                </div>
+
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+        </div> 
+
+</div>
                         
 
         </div> 
